0b0af6c2de06ee10245cb59773dce33b372cfc83
Paul Chin Jr <pchinjr@gmail.com>
2021-03-15 20:18:14 -0400

    reduced to find bug

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const fetch = require('node-fetch');
 
 exports.handler = async function http(req) {
 
-  let url = `https://api.amc.husqvarna.dev/v1/mowers/${process.env.MOWER_ID}`
+  // let url = `https://api.amc.husqvarna.dev/v1/mowers/${process.env.MOWER_ID}`
 
-  let options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${process.env.AUTH_BEARER_TOKEN}`,
-      'X-Api-Key': `${process.env.API_KEY}`,
-      'Authorization-Provider': 'husqvarna'
-    }
-  }
+  // let options = {
+  //   method: 'GET',
+  //   headers: {
+  //     Authorization: `Bearer ${process.env.AUTH_BEARER_TOKEN}`,
+  //     'X-Api-Key': `${process.env.API_KEY}`,
+  //     'Authorization-Provider': 'husqvarna'
+  //   }
+  // }
 
-  let data
+  // let data
 
-  await fetch(url, options)
-    .then(res => res.json())
-    .then(json => response(json))
-    .catch(err => console.error('error:' + err));
+  // await fetch(url, options)
+  //   .then(res => res.json())
+  //   .then(json => response(json))
+  //   .catch(err => console.error('error:' + err));
 
-  async function response(json) {
-    console.log(json.data)
-    data = json.data
-  }
+  // async function response(json) {
+  //   console.log(json.data)
+  //   data = json.data
+  // }
 
   return {
     headers: {
@@ -31,6 +31,6 @@ exports.handler = async function http(req) {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: `praise cage`
   }
 }
